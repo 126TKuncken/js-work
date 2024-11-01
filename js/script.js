8c26d6c137925d1daa838f438a89a16cd81e26bd
Tyler Kuncken <126TKuncken@frhsd.com>
2024-11-01 12:02:22 -0400

    finished

--- js/script.js
+++ js/script.js
@@ -9,19 +9,9 @@ if (hour < 12) {
   greetingText.textContent = "Good Morning!";
   document.querySelector(".night").classList.add("d-none");
   document.querySelector(".afternoon").classList.add("d-none");
-} else if (hour < 17) {
+ } else if (hour < 17){
+  
+ }
+
  
     
-  greetingText.textContent = "Good Afternoon!";
-  document.getElementsByTagName("body")[0].classList.add("noon-bg");
-  greetingText.parentElement.classList.add("noon-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".night").classList.add("d-none");
-} 
-
-else {
-  greetingText.textContent = "Good Night!";
-  greetingText.parentElement.classList.add("night-gradient");
-  document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
-}
